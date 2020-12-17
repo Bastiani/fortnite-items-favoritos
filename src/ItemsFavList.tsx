48a0a07ec1238f184b340aa00ba472a7fa98e0a6
Rafael de Bastiani <rafacdb@gmail.com>
2020-12-17 10:50:02 -0300

    Add icon into list fav

--- src/ItemsFavList.tsx
+++ src/ItemsFavList.tsx
@@ -1,5 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { OrderedList, ListItem, Link, Button } from "@chakra-ui/react";
+import {
+  OrderedList,
+  ListItem,
+  Link,
+  Button,
+  Flex,
+  Image
+} from "@chakra-ui/react";
 import { DeleteIcon } from "@chakra-ui/icons";
 
 import { cosmeticsById } from "./api";
@@ -26,24 +33,34 @@ const ItemsFavList = () => {
     <OrderedList p="10px">
       {itemsState.map((item: any) => (
         <ListItem key={item.data.items[0].id} mb="5px">
-          <Link
-            href={
-              item.data.items[0].images.featured ||
-              item.data.items[0].images.icon
-            }
-            isExternal
-          >
-            {item.data.items[0].name}
-          </Link>
-          <Button
-            w={6}
-            h={6}
-            onClick={() => {
-              dispatch({ type: "remove", value: item.data.items[0].id });
-            }}
-          >
-            <DeleteIcon />
-          </Button>
+          <Flex direction="column" m="15px">
+            <Link
+              href={
+                item.data.items[0].images.featured ||
+                item.data.items[0].images.icon
+              }
+              isExternal
+            >
+              <Flex direction="row" mb="10px" alignItems="center">
+                <Image
+                  key={item.data.items[0].id}
+                  src={item.data.items[0].images.icon}
+                  alt={item.data.items[0].description}
+                  boxSize="50px"
+                />
+                {item.data.items[0].name}
+              </Flex>
+            </Link>
+            <Button
+              w={6}
+              h={6}
+              onClick={() => {
+                dispatch({ type: "remove", value: item.data.items[0].id });
+              }}
+            >
+              <DeleteIcon />
+            </Button>
+          </Flex>
         </ListItem>
       ))}
     </OrderedList>
